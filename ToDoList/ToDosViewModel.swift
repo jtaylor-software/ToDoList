ef3a778bf7b53086a7c88cd688611c989474948e
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-11 13:27:03 -0600

    Persist toDos array by saving json data locally on device

--- ToDoList/ToDosViewModel.swift
+++ ToDoList/ToDosViewModel.swift
@@ -11,9 +11,7 @@ class ToDosViewModel: ObservableObject {
     @Published var toDos: [ToDo] = []
     
     init() {
-        toDos.append(ToDo(id: UUID().uuidString, item: "Learn Swift"))
-        toDos.append(ToDo(id: UUID().uuidString, item: "Build Apps"))
-        toDos.append(ToDo(id: UUID().uuidString, item: "Change the World!"))
+        loadData()
     }
     
     func saveToDo(toDo: ToDo) {
@@ -27,13 +25,36 @@ class ToDosViewModel: ObservableObject {
                 toDos[index] = toDo
             }
         }
+        saveData()
     }
     
     func deleteToDo(indexSet: IndexSet) {
         toDos.remove(atOffsets: indexSet)
+        saveData()
     }
     
     func moveToDo(fromOffsets: IndexSet, toOffset: Int) {
         toDos.move(fromOffsets: fromOffsets, toOffset: toOffset)
+        saveData()
     }
-}
+    
+    func loadData() {
+        let path = URL.documentsDirectory.appending(component: "toDos")
+        guard let data = try? Data(contentsOf: path) else { return }
+        do {
+            toDos = try JSONDecoder().decode([ToDo].self, from: data)
+        } catch {
+            print("😡 ERROR: Could not load data \(error.localizedDescription)")
+        }
+    }
+        
+        func saveData() {
+            let path = URL.documentsDirectory.appending(component: "toDos")
+            let data = try? JSONEncoder().encode(toDos) // try? means if error is thrown, data = nil
+            do {
+                try data?.write(to: path, options: .completeFileProtection)
+            } catch {
+                print("😡 ERROR: Could not save data \(error.localizedDescription)")
+            }
+        }
+    }
