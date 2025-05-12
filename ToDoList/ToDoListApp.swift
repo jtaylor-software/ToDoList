858af078c731134bb1a725e6c49ce62611eac92d
Jeremy Taylor <jtaylor@jctaylor.org>
2025-05-12 08:29:20 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jeremy Taylor on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
